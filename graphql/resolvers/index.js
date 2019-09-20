1b06a777d91ff592ed819c64df9f4dba974b71a1
tongzcomradez <s.rotbamrung@gmail.com>
2019-09-20 14:06:26 +0700

    test

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,10 +1,46 @@
 const todoResolver = require('./todo')
 
+const books = [
+  {
+    id: '1',
+    title: 'Harry potter',
+    authorId: '1'
+  },
+  {
+    id: '2',
+    title: 'Harry potter 2',
+    authorId: '2'
+  },
+]
+
+const authors = [
+  {
+    id: '1',
+    name: 'JK Rolling'
+  },
+  {
+    id: '2',
+    name: 'Stone'
+  },
+]
+
 module.exports = {
+  Book: {
+    author: (parent) => {
+      console.log(parent.authorId)
+      return authors.find(a => a.id === parent.authorId)
+    }
+  },
   Query: {
     ...todoResolver.Query,
+    books: (parent, args) => {
+      return books
+    }
   },
   Mutation: {
     ...todoResolver.Mutation,
   },
+  Subscription: {
+    ...todoResolver.Subscription,
+  }
 }
--- graphql/resolvers/todo.js
+++ graphql/resolvers/todo.js
@@ -1,4 +1,7 @@
 const Todo = require('../../models/Todo')
+const { PubSub } = require('apollo-server');
+
+const pubsub = new PubSub();
 
 module.exports = {
   Query: {
@@ -19,12 +22,25 @@ module.exports = {
         createdAt: new Date(),
       }).save()
       // todo pubsub
+      pubsub.publish('createTodo', { createTodo: todo });
+
       return todo
     },
     updateTodo: async (_, args) => {
-      const todo = await Todo.findByIdAndUpdate(args.id, { status: 'complete' })
+      const todo = await Todo.findByIdAndUpdate(args.id, { status: 'complete' }, { new: true })
       console.log(todo)
       return todo
     },
+    deleteTodo: async (_, args) => {
+      console.log(args.id)
+      const todo = await Todo.findById(args.id)
+      todo.delete()
+      return args.id.toString()
+    },
   },
+  Subscription: {
+    createTodo: {
+      subscribe: () => pubsub.asyncIterator(['createTodo']),
+    }
+  }
 }
